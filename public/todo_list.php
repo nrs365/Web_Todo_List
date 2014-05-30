db4aff76e3fb0da7a67139cceb0aac4de1a33d4d
nrs365 <nrs365@gmail.com>
2014-05-30 15:11:21 -0500

    added function to upload file and have it merge with existing todo list

--- public/todo_list.php
+++ public/todo_list.php
@@ -20,25 +20,22 @@ $filename = 'data/list.txt';
 
 		function save_file($filename, $array) {
    			$saved_file = fopen($filename, 'w');
-		    //$arrayNew = array_merge($saved_file, $array);
     		$listString = implode("\n", $array);
     		fwrite($saved_file, $listString);
     		fclose($saved_file);
-    		//return "Thanks for saving to {$filename}!\n";
 		}
 
-		// echo "<h4>GET</h4>";
-		// echo $_GET['name'];
-		// echo $_GET['key'];
-
-		// echo "<h4>POST</h4>";
-		// echo $_POST['add'];
-		// echo $_POST['open'];
-
+		function upload_file ($uploaded_file) {
+			$filename = $uploaded_file['name'];
+			$upload_directory = '/vagrant/sites/todo.dev/public/uploads/';
+			$upload_filename = basename($filename);
+			$saved_filename = $upload_directory . $upload_filename;
+			move_uploaded_file($uploaded_file['tmp_name'], $saved_filename);
+			return $saved_filename;
+		}
 	?>
 	<h2>TODO List</h2>
 	<?php
-
 		function display_list($array) {
 			echo "<ul>";
 			foreach ($array as $key => $item) {
@@ -47,37 +44,45 @@ $filename = 'data/list.txt';
 			echo "</ul>";
 		}
 	?>
-
-	<?php	
+	<?php
+		var_dump($_POST);
+		var_dump($_FILES['file1']);
 		$list = open_file($filename);
 		if (isset($_POST['add']) || !empty($_POST['add'])) {
 			array_push($list, $_POST['add']);
 			save_file($filename, $list);
 			display_list($list);
-		}
-		 else if (isset($_GET['key']) || !empty($_GET['key'])) {
-			//$key_to_remove = $_GET['key'];
+		} else if (isset($_GET['key']) || !empty($_GET['key'])) {
 			unset($list[$_GET['key']]);
 			save_file($filename, $list);
 			display_list($list);
-		}
-		else
-		{
+		} else if (isset($_FILES['file1']['error']) && ($_FILES['file1']['error'] == 0)) {
+			//if ($_FILES['file1']['type'] )
+			$saved_filename = upload_file($_FILES['file1']);
+			$saved_file_array = open_file($saved_filename);//saved file name from function
+			$list = array_merge($list, $saved_file_array);
+			save_file($filename, $list);
+			display_list($list);
+		} else {
 			display_list($list);
 		}
 	?>
-	<form method="POST" action="todo_list.php">
+	<form method="POST" action="/todo_list.php">
 		<p>
 			<label for="add">Type in something to add to the list: </label>
 			<input type="text" id="add" name="add">
 		</p>
 		<p>
-			<label for="open">Type in a file you would like to open and add to the list: </label>
-			<input type="text" id="open" name="open">
-		</p>
-		<p>
 			<button type="submit">Add it!</button>
 		</p>		
+	</form>	
+	<form method="POST" enctype="multipart/form-data" action="/todo_list.php">
+		<p>
+			<label for="file1">Type in a file you would like to open and add to the list: </label>
+			<input type="file" id="file1" name="file1">
+		</p>
+		<p>
+			<button type="submit" value="upload">Upload file!</button>
 	</form>	
 </body>
 </html>
