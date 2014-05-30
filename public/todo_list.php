b0f8eedc097f2e79f5639d9e3fb1f689637940fc
nrs365 <nrs365@gmail.com>
2014-05-29 19:12:19 -0500

    finished browser todo list that allows the user to add and delete from list

--- public/todo_list.php
+++ public/todo_list.php
@@ -1,45 +1,79 @@
-<!DOCTYPE html>
+<?php 
+$filename = 'data/list.txt';
+?>
 
+<!DOCTYPE html>
 <html>
 <head>
 	<title>Todo List</title>
 </head>
 <body>
 	<?php
-		function open_file($filename, $array) {
+		function open_file($filename) {
 		    $filesize = filesize($filename);
 		    $read = fopen($filename, 'r');
 		    $listString = trim(fread($read, $filesize));
 		    $listArray = explode("\n", $listString);
-		    $array = array_merge($listArray, $array);
-		    fclose($read);
-		    return $array;        
+    		fclose($read);
+    		return $listArray;               
 		}
 
-		echo "<h4>GET</h4>";
-		var_dump($_GET);
-		echo "<h4>POST</h4>";
-		var_dump($_POST);
-	?>
-		<h2>TODO List</h2>
-		<?php 
-			$list = ["fixed cracked Mac screen", "count how many stars are out", "Get something for Libby's graduation"];
+		function save_file($filename, $array) {
+   			$saved_file = fopen($filename, 'w');
+		    //$arrayNew = array_merge($saved_file, $array);
+    		$listString = implode("\n", $array);
+    		fwrite($saved_file, $listString);
+    		fclose($saved_file);
+    		//return "Thanks for saving to {$filename}!\n";
+		}
 
-			// for($i = 0; $i <= count($list); $i++) {
-			// 	echo "<li>$list</li>";
-			// }
+		// echo "<h4>GET</h4>";
+		// echo $_GET['name'];
+		// echo $_GET['key'];
+
+		// echo "<h4>POST</h4>";
+		// echo $_POST['add'];
+		// echo $_POST['open'];
+
+	?>
+	<h2>TODO List</h2>
+	<?php
+
+		function display_list($array) {
 			echo "<ul>";
-			foreach ($list as $key => $item) {
-				echo "<li>$item</li>";
+			foreach ($array as $key => $item) {
+				echo "<li>$item <a href=\"?key=$key\">Complete</a></li>"; //?action=remove&index={$index}
 			}
 			echo "</ul>";
-		?>
-	
+		}
+	?>
 
-	<form method="POST">
-		<p>	
+	<?php	
+		$list = open_file($filename);
+		if (isset($_POST['add']) || !empty($_POST['add'])) {
+			array_push($list, $_POST['add']);
+			save_file($filename, $list);
+			display_list($list);
+		}
+		 else if (isset($_GET['key']) || !empty($_GET['key'])) {
+			//$key_to_remove = $_GET['key'];
+			unset($list[$_GET['key']]);
+			save_file($filename, $list);
+			display_list($list);
+		}
+		else
+		{
+			display_list($list);
+		}
+	?>
+	<form method="POST" action="todo_list.php">
+		<p>
 			<label for="add">Type in something to add to the list: </label>
 			<input type="text" id="add" name="add">
+		</p>
+		<p>
+			<label for="open">Type in a file you would like to open and add to the list: </label>
+			<input type="text" id="open" name="open">
 		</p>
 		<p>
 			<button type="submit">Add it!</button>
