64eaa68d91ef7d0a487dcf9fdc22aaa5d504950e
nrs365 <nrs365@gmail.com>
2014-06-19 11:37:08 -0500

    refactored code to use a class filestore linked file

--- public/todo_list.php
+++ public/todo_list.php
@@ -1,72 +1,80 @@
-<?
+<?php
+
+require_once('classes/filestore.php');
 
 $filename = 'data/list.txt';
 
-function open_file($filename) {
-	$list_array = [];
-	if (is_readable($filename) && filesize($filename) > 0) {
-		$filesize = filesize($filename);
-   		$read = fopen($filename, 'r');
-   		$list_string = trim(fread($read, $filesize));
-   		$list_array = explode("\n", $list_string);
-   		fclose($read);
-   	}
-	return $list_array;		               
-}
 
-function save_file($filename, $array) {
-	$saved_file = fopen($filename, 'w');
-	$list_string = implode("\n", $array);
-	fwrite($saved_file, $list_string);
-	fclose($saved_file);
-}
+// //need to instanciate the class for Filestore
+$filestore = New Filestore($filename);
+$list = $filestore->read_lines();
+//var_dump($list);
 
-function upload_file ($uploaded_file) {
-	$filename = $uploaded_file['name'];
-	$upload_directory = '/vagrant/sites/todo.dev/public/uploads/';
-	$upload_filename = basename($filename);
-	$saved_filename = $upload_directory . $upload_filename;
-	move_uploaded_file($uploaded_file['tmp_name'], $saved_filename);
-	return $saved_filename;
-}
+// function open_file($filename) {
+// 	$list_array = [];
+// 	if (is_readable($filename) && filesize($filename) > 0) {
+// 		$filesize = filesize($filename);
+//    		$read = fopen($filename, 'r');
+//    		$list_string = trim(fread($read, $filesize));
+//    		$list_array = explode("\n", $list_string);
+//    		fclose($read);
+//    	}
+// 	return $list_array;		               
+// }
 
-$list = open_file($filename);
+// function save_file($filename, $array) {
+// 	$saved_file = fopen($filename, 'w');
+// 	$list_string = implode("\n", $array);
+// 	fwrite($saved_file, $list_string);
+// 	fclose($saved_file);
+// }
+
+// function upload_file ($uploaded_file) {
+// 	$filename = $uploaded_file['name'];
+// 	$upload_directory = '/vagrant/sites/todo.dev/public/uploads/';
+// 	$upload_filename = basename($filename);
+// 	$saved_filename = $upload_directory . $upload_filename;
+// 	move_uploaded_file($uploaded_file['tmp_name'], $saved_filename);
+// 	return $saved_filename;
+// }
+
+//"<li>$item <a href=\"?key=$key\">Complete</a></li>";
+$list = $filestore->read_lines($filename);
 if (isset($_POST['add']) || !empty($_POST['add'])) {
 	array_push($list, $_POST['add']);
-	save_file($filename, $list);
+	$filestore->write_lines($list);
 
 } else if (isset($_GET['key']) || !empty($_GET['key'])) {
 	unset($list[$_GET['key']]);
-	save_file($filename, $list);
+	$filestore->write_lines($list);
 
 } else if (isset($_FILES['file1']['error']) && ($_FILES['file1']['error'] == 0)) {
 	if ($_FILES['file1']['type'] == 'text/plain') {
 		$saved_filename = upload_file($_FILES['file1']);
 		$saved_file_array = open_file($saved_filename);
 		$list = array_merge($list, $saved_file_array);
-		save_file($filename, $list);
-	} else {
+		$filestore->write_lines($list);
+} else {
 		echo "You cannot upload that file";
 	}
 }
-
+//var_dump($list);
 ?>
+
 <!DOCTYPE html>
 <html>
 <head>
 	<meta charset="UTF-8">
 	<title>ToDo List</title>
-	<link rel="stylesheet" href="/todo_list.css">
 </head>
 <body>
 	<h2>ToDo List</h2>
-	
 	<ul>
 	<? foreach ($list as $key => $item) : ?>
-		<?= "<li>$item <a href=\"?key=$key\">Complete</a></li>"; ?>
+
+		<li><?= $item ?><a href="?key=<?= $key; ?>">Complete</a></li>
 	<? endforeach; ?>
 	</ul>
-
 	<form method="POST" action="/todo_list.php">
 		<p>
 			<label for="add">Type in something to add to the list: </label>
@@ -83,6 +91,7 @@ if (isset($_POST['add']) || !empty($_POST['add'])) {
 		</p>
 		<p>
 			<button type="submit" value="upload">Upload file!</button>
+		</p>	
 	</form>	
 </body>
 </html>
